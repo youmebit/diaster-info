3699f90e8e9e9e44ef848a6999fc7eec960dc65b
紅山藍子 <bennie.a.320@gmail.com>
2016-04-07 19:18:16 +0900

    401エラーが出たのでコメントアウト

--- www/js/top.js
+++ www/js/top.js
@@ -1,21 +1,21 @@
 'use strict';
 app.controller('topCtrl', function($scope, mBaasService) {
     $scope.settings.isHiddenTab = false;
-    if ($scope.isLogin) {
-        var user = mBaasService.getCurrentUser();
-        $scope.username = user.userName;
-        user.logout();
-    } else {
-        $scope.username = 'ゲスト';
-        var address = 'saltory72@gmail.com';
-        var password = 'password';
-        var ncmb = mBaasService.getNcmb();
-        ncmb.User.loginWithMailAddress(address, password).then(function(data){
-            console.log('ログインしました');
-        })
-        .catch(function(err){
-            alert('失敗');
-            console.log(err);
-        });
-    }
+//    if ($scope.isLogin) {
+//        var user = mBaasService.getCurrentUser();
+//        $scope.username = user.userName;
+//        user.logout();
+//    } else {
+//        $scope.username = 'ゲスト';
+//        var address = 'saltory72@gmail.com';
+//        var password = 'password';
+//        var ncmb = mBaasService.getNcmb();
+//        ncmb.User.loginWithMailAddress(address, password).then(function(data){
+//            console.log('ログインしました');
+//        })
+//        .catch(function(err){
+//            alert('失敗');
+//            console.log(err);
+//        });
+//    }
 });
