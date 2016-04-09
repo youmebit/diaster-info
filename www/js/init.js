1902c284a97ac426f08213dd041a62ae85bb4a27
紅山藍子 <bennie.a.320@gmail.com>
2016-04-09 10:53:46 +0900

    読み込み中画面を作成。

--- www/js/init.js
+++ www/js/init.js
@@ -5,6 +5,25 @@ app.controller('bodyCtrl', function($scope, mBaasService) {
     $scope.settings.isHiddenTab = false;
     // ログイン機能仮置き
 //    mBaasService.login('saltory72@gmail.com', 'password');
+    $scope.topInit = function() {
+        $scope.settings.isHiddenTab = true;
+        $scope.user = {};
+        $scope.isLogin = false;
+        var current = mBaasService.getCurrentUser();
+        if (current) {
+            var ncmb = mBaasService.getNcmb();
+            mBaasService.login(current.mailAddress, current.password);
+            $scope.$on('auto_login', function(event, data) {
+                $scope.$apply(function () {
+                    $scope.user.username = data;
+                    ncmb.User.logout();
+                    $scope.isLogin = true;
+                });
+            });
+        } else {
+            $scope.user.username = 'ゲスト';
+        }
+    }
 });
 
 app.service('mBaasService', function ($rootScope) {
