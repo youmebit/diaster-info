f2f133b042bf1c1846458bae2d2653acd937631d
紅山藍子 <bennie.a.320@gmail.com>
2016-04-10 16:29:45 +0900

    タブの切り替えを作成。ref #17

--- www/js/post.js
+++ www/js/post.js
@@ -1,3 +1,9 @@
-app.controller('postCtrl', function() {
+app.controller('postCtrl', function($scope) {
+    $scope.showCamera = function() {
+        $scope.isCamera = true;
+    }
     
+    $scope.showGallery = function() {
+        $scope.isCamera = false;
+    }
 });
