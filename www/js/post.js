dabe8c9362d8447251041f90c2a7e9aa4bd687b6
紅山藍子 <bennie.a.320@gmail.com>
2016-04-10 17:18:41 +0900

    タブの切り替え時とギャラリーのレイアウトを追加。

--- www/js/post.js
+++ www/js/post.js
@@ -5,5 +5,6 @@ app.controller('postCtrl', function($scope) {
     
     $scope.showGallery = function() {
         $scope.isCamera = false;
+        $scope.images = ['http://lorempixel.com/100/100/animals', 'http://lorempixel.com/100/100/cats', 'http://lorempixel.com/100/100/fashion', 'http://lorempixel.com/100/100/people', 'http://lorempixel.com/100/100/city', 'http://lorempixel.com/100/100/nightlife', 'http://lorempixel.com/100/100/food','http://lorempixel.com/100/100/sports'];
     }
 });
