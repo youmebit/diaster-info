830bb55a16eafe3de68a7b5dfe93e73def396004
紅山藍子 <bennie.a.320@gmail.com>
2016-04-11 10:01:22 +0900

    カメラ機能を追加。ref #33

--- www/js/post.js
+++ www/js/post.js
@@ -1,32 +1,28 @@
 app.controller('postCtrl', function($scope) {
     $scope.showCamera = function() {
-        $scope.isCamera = true;
-        myNavigator.pushPage('post/post.html');
-
-//        var options = {
-//            quality : 75,
-//           destinationType : Camera.DestinationType.FILE_URI,
-//           sourceType : Camera.PictureSourceType.CAMERA,
-//            saveToPhotoAlbum : true,
-//            correntOrientation : true,
-//            cameraDirection : Camera.Direction.BACK
-//        }
-//        
-//        var onSuccess = function(imageURI) {
-//            console.log(imageURI);
-//            navigator.pushPage('post/post.html');
-//        }
-//        
-//        var onFail = function() {
-//            console.log('写真失敗');
-//        }
-//        navigator.camera.getPicture(function(imageURI) {
-//            onSuccess(imageURI);
-//        }, onFail, options);
+        var options = {
+            quality : 75,
+           destinationType : Camera.DestinationType.FILE_URI,
+           sourceType : Camera.PictureSourceType.CAMERA,
+            saveToPhotoAlbum : true,
+            correntOrientation : true,
+            cameraDirection : Camera.Direction.BACK
+        }
+        
+        var onSuccess = function(imageURI) {
+            console.log(imageURI);
+            myNavigator.pushPage('post/post.html');
+        }
+        
+        var onFail = function() {
+            console.log('写真失敗');
+        }
+        navigator.camera.getPicture(function(imageURI) {
+            onSuccess(imageURI);
+        }, onFail, options);
     }
     
     $scope.showGallery = function() {
-        $scope.isCamera = false;
         $scope.images = ['http://lorempixel.com/100/100/animals', 'http://lorempixel.com/100/100/cats', 'http://lorempixel.com/100/100/fashion', 'http://lorempixel.com/100/100/people', 'http://lorempixel.com/100/100/city', 'http://lorempixel.com/100/100/nightlife', 'http://lorempixel.com/100/100/food','http://lorempixel.com/100/100/sports'];
     }
 });
