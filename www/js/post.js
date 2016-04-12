e730668a5f74e48d0df2bff895e3f877b476e694
紅山藍子 <bennie.a.320@gmail.com>
2016-04-12 20:38:17 +0900

    アップロード機能を追加。(androidできない)

--- www/js/post.js
+++ www/js/post.js
@@ -1,8 +1,8 @@
-app.controller('imgSelectCtrl', function ($scope) {
+app.controller('imgSelectCtrl', function ($scope, mBaasService) {
     $scope.showCamera = function () {
         var options = {
             quality: 70,
-            destinationType: Camera.DestinationType.FILE_URI,
+            destinationType: Camera.DestinationType.DATA_URL,
             sourceType: Camera.PictureSourceType.CAMERA,
             saveToPhotoAlbum: true,
             correntOrientation: true,
@@ -18,10 +18,6 @@ app.controller('imgSelectCtrl', function ($scope) {
         }
 
         getPicture(options, onSuccess);
-
-        //        navigator.camera.getPicture(function (imageURI) {
-        //            onSuccess(imageURI);
-        //        }, onFail, options);
     }
 
     $scope.showGallery = function () {
@@ -33,11 +29,21 @@ app.controller('imgSelectCtrl', function ($scope) {
         }
 
         var onSuccess = function (imageURI) {
-            myNavigator.pushPage('post.html', {
-                image: "data:image/jpeg;base64," + imageURI
-            });
-        }
 
+            var blob = toBlob(imageURI);
+            var ncmb = mBaasService.getNcmb();
+            ncmb.File.upload('aa.jpg', blob).then(
+                function (data) {
+                    console.log('できたー');
+                    myNavigator.pushPage('post.html', {
+                        image: data.uri
+                    });
+                }
+            ).catch(function (err) {
+                console.error(err);
+            });
+
+        }
         getPicture(options, onSuccess);
     }
 
@@ -51,6 +57,36 @@ app.controller('imgSelectCtrl', function ($scope) {
         navigator.camera.getPicture(function (imageURI) {
             onSuccess(imageURI);
         }, onFail, options);
+    }
+
+    function toBlob(base64) {
+        base64 = "data:image/jpeg;base64," + base64;
+        var bin = atob(base64.replace(/^.*,/, ''));
+        var buffer = new Uint8Array(bin.length);
+        for (var i = 0; i < bin.length; i++) {
+            buffer[i] = bin.charCodeAt(i);
+        }
+        // Blobを作成
+        try {
+            var blob = new Blob([buffer], {
+                type: 'image/jpg'
+            });
+        } catch (e) {
+            window.BlobBuilder = window.BlobBuilder || 
+                                     window.WebKitBlobBuilder || 
+                                     window.MozBlobBuilder || 
+                                     window.MSBlobBuilder;
+            if(e.name == 'TypeError' && window.BlobBuilder){
+                var bb = new (window.BlobBuilder)();
+                console.log(buffer[0]);
+                bb.append([buffer]);
+                blob = bb.getBlob("image/jpg");
+            } else {
+                console.error(err);
+                return false;
+            }
+        }
+        return blob;
     }
 });
 
