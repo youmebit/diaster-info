7d9ff75c16d12153551184d32e78efbfe095be69
紅山藍子 <bennie.a.320@gmail.com>
2016-04-12 15:36:39 +0900

    ギャラリーから画像を取得する処理を追加。

--- www/js/post.js
+++ www/js/post.js
@@ -1,32 +1,60 @@
-app.controller('imgSelectCtrl', function($scope) {
-    $scope.showCamera = function() {
+app.controller('imgSelectCtrl', function ($scope) {
+    $scope.showCamera = function () {
         var options = {
-            quality : 75,
-           destinationType : Camera.DestinationType.FILE_URI,
-           sourceType : Camera.PictureSourceType.CAMERA,
-            saveToPhotoAlbum : true,
-            correntOrientation : true,
-            cameraDirection : Camera.Direction.BACK
+            quality: 70,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            saveToPhotoAlbum: true,
+            correntOrientation: true,
+            encodingType: Camera.EncodingType.JPEG,
+            cameraDirection: Camera.Direction.BACK
         }
-        
-        var onSuccess = function(imageURI) {
-            myNavigator.pushPage('post.html', {image: imageURI});
+
+        var onSuccess = function (imageURI) {
+            console.log(imageURI);
+            myNavigator.pushPage('post.html', {
+                image: imageURI
+            });
         }
-        
-        var onFail = function() {
-            console.log('写真失敗');
+
+        getPicture(options, onSuccess);
+
+        //        navigator.camera.getPicture(function (imageURI) {
+        //            onSuccess(imageURI);
+        //        }, onFail, options);
+    }
+
+    $scope.showGallery = function () {
+        var options = {
+            quality: 70,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            encodingType: Camera.EncodingType.JPEG
         }
-        navigator.camera.getPicture(function(imageURI) {
+
+        var onSuccess = function (imageURI) {
+            myNavigator.pushPage('post.html', {
+                image: "data:image/jpeg;base64," + imageURI
+            });
+        }
+
+        getPicture(options, onSuccess);
+    }
+
+    // ギャラリーorカメラから画像を投稿フォームに表示する。
+    function getPicture(options, onSuccess) {
+
+        var onFail = function () {
+            console.error('画像の取得失敗');
+        }
+
+        navigator.camera.getPicture(function (imageURI) {
             onSuccess(imageURI);
         }, onFail, options);
     }
-    
-    $scope.showGallery = function() {
-//        $scope.images = ['http://lorempixel.com/100/100/animals', 'http://lorempixel.com/100/100/cats', 'http://lorempixel.com/100/100/fashion', 'http://lorempixel.com/100/100/people', 'http://lorempixel.com/100/100/city', 'http://lorempixel.com/100/100/nightlife', 'http://lorempixel.com/100/100/food','http://lorempixel.com/100/100/sports'];
-    }
 });
 
-app.controller('postCtrl', function($scope) {
+app.controller('postCtrl', function ($scope) {
     var options = $scope.myNavigator.getCurrentPage().options;
     $scope.imageURI = options.image;
 });
