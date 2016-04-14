34904f1991e55bb0b994c96a3a882c7c6ae737d1
紅山藍子 <bennie.a.320@gmail.com>
2016-04-14 10:33:42 +0900

    画像の縮小サイズを変更。

--- www/js/post.js
+++ www/js/post.js
@@ -60,18 +60,18 @@ app.controller('postCtrl', function ($scope) {
     // 画像縮小処理
     $scope.init = function () {
         var image = new Image();
-        image.onload = function(e) {
-            $scope.$apply(function() {
+        image.onload = function (e) {
+            $scope.$apply(function () {
                 var imgWidth = image.naturalWidth;
                 var imgHeight = image.naturalHeight;
                 var rate = 0;
                 if (imgWidth >= imgHeight) {
-                    rate = 320 / imgWidth;
+                    rate = 540 / imgWidth;
                 } else {
-                    rate = 320 / imgHeight;
+                    rate = 540 / imgHeight;
                 }
 
-                EXIF.getData(image, function() {
+                EXIF.getData(image, function () {
                     var canvas = document.createElement('canvas');
                     var drawWidth = imgWidth * rate;
                     var drawHeight = imgHeight * rate;
@@ -80,7 +80,11 @@ app.controller('postCtrl', function ($scope) {
                     var ctx = canvas.getContext('2d');
                     var orientation = EXIF.getTag(image, "Orientation");
                     console.log(orientation);
-                    var angles = {'3':180, '6':90, '8': 270};
+                    var angles = {
+                        '3': 180,
+                        '6': 90,
+                        '8': 270
+                    };
                     ctx.translate(drawWidth / 2, drawHeight / 2);
                     ctx.rotate((angles[orientation] * Math.PI) / 180);
                     ctx.translate(-drawWidth / 2, -drawHeight / 2);
@@ -90,47 +94,58 @@ app.controller('postCtrl', function ($scope) {
 
             })
         }
-        
         var options = $scope.myNavigator.getCurrentPage().options;
         image.src = options.image;
     }
 
-    $scope.resize = function() {
-    }
     $scope.upload = function () {
-        var img = document.getElementById('upload_img');
-        alert(img.width);
+        alert(getFileName());
+//        var blob = b64ToBlob($scope.imageURI);
+//        var ncmb = mBaasService.getNcmb();
+//        ncmb.File.upload(Date.now() + '.jpg', blob).then(
+//            function (data) {
+//                console.log('できたー');
+//                myNavigator.pushPage('post.html', {
+//                    image: data.uri
+//                });
+//            }
+//        ).catch(function (err) {
+//            console.error(err);
+//        });
     }
 });
 
-app.directive('imgOnload', ['$parse', function ($parse) {
-    return {
-      link: function(scope, element, attrs) {
-        element.bind("load" , function(e) {
-          var func = $parse(attrs.imgOnload);
-          func(scope);
-        });
-      }
-    }
-  }]);
-
-// base64形式の画像データをBlobオブジェクトに変換する。
-function toBlob(canvas) {
-    if (canvas && canvas.getContext) {
-        var imageType = 'image/jpg';
-        var base64 = canvas.toDataURL(imageType);
-        var bin = atob(base64.replace(/^.*,/, ''));
-        var buffer = new Uint8Array(bin.length);
-        for (var i = 0; i < bin.length; i++) {
-            buffer[i] = bin.charCodeAt(i);
-        }
-
-        return new Blob([buffer.buffer], {
-            type: imageType});
-    }
-    return null;
+function getFileName() {
+    var date = new Date();
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    var h = date.getHours();
+    var mi = date.getMinutes();
+    var s = date.getSeconds();
+    return y + padZero(m) + padZero(d) + padZero(h) + padZero(mi) + padZero(s) + ".jpg";
 }
 
+function padZero(value) {
+    return ('0' + value).slice(-2);
+}
+//function toBlob(canvas) {
+//    if (canvas && canvas.getContext) {
+//        var imageType = 'image/jpg';
+//        var base64 = canvas.toDataURL(imageType);
+//        var bin = atob(base64.replace(/^.*,/, ''));
+//        var buffer = new Uint8Array(bin.length);
+//        for (var i = 0; i < bin.length; i++) {
+//            buffer[i] = bin.charCodeAt(i);
+//        }
+//
+//        return new Blob([buffer.buffer], {
+//            type: imageType});
+//    }
+//    return null;
+//}
+
+// base64形式の画像データをBlobオブジェクトに変換する。
 function b64ToBlob(base64) {
     var bin = atob(base64.replace(/^.*,/, ''));
     var buffer = new Uint8Array(bin.length);
@@ -138,11 +153,11 @@ function b64ToBlob(base64) {
         buffer[i] = bin.charCodeAt(i);
     }
     // Blobを作成
-    try{
+    try {
         var blob = new Blob([buffer.buffer], {
             type: 'image/jpg'
         });
-    }catch (e){
+    } catch (e) {
         return false;
     }
     return blob;
