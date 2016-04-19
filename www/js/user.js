48e9000dd6eb375a3162d79c8ef762261420efbe
紅山藍子 <bennie.a.320@gmail.com>
2016-04-19 11:12:25 +0900

    会員へのロール追加処理を追加。ref #38

--- www/js/user.js
+++ www/js/user.js
@@ -5,13 +5,27 @@ app.controller('userCtrl', function ($scope, mBaasService) {
         if (!confirm('登録してもよろしいですか?')) {
             return false;
         }
-        var ncmb = mBaasService.getNcmb();
-        var user = new ncmb.User();
-        user.set("userName", $scope.signup.username)
-            .set("password", $scope.signup.password)
-            .set("mailAddress", $scope.signup.email);
 
-        user.signUpByAccount()
+        var ncmb = mBaasService.getNcmb();
+        ncmb.Role.equalTo("roleName", "member").fetch()
+        .then(function(result){
+            addUser(result);
+        })
+        .catch(function(err) {
+            var role = new ncmb.Role("member");
+            role.save().then(function() {
+                addUser(role);
+            });
+        });
+        
+        var addUser = function(role) {
+            var ncmb = mBaasService.getNcmb();
+            var user = new ncmb.User();
+            user.set("userName", $scope.signup.username)
+                .set("password", $scope.signup.password)
+                .set("mailAddress", $scope.signup.email)
+                .set("role", role);
+            user.signUpByAccount()
             .then(function () {
                 myNavigator.pushPage('user/regist_info.html');
             })
@@ -22,5 +36,6 @@ app.controller('userCtrl', function ($scope, mBaasService) {
                 }
                 alert(message);
             });
+        }
     }
 });
