d8c88a5e26b347dad9397a1c6fc5397e6f467729
紅山藍子 <bennie.a.320@gmail.com>
2016-04-20 19:38:58 +0900

    詳細画面のタブ表示を追加。ref #22

--- www/js/display.js
+++ www/js/display.js
@@ -1,3 +1,24 @@
 'use strict';
 app.controller('displayCtrl', function($scope) {
 });
+
+app.controller('detailCtrl', function($scope) {
+	$scope.init = function() {
+		$scope.tab = 'map';
+	}
+	
+	// 地図タブ表示
+	$scope.toMap = function() {
+		$scope.tab = 'map';
+	}
+	
+	// コメントタブ表示
+	$scope.toComment = function() {
+		$scope.tab = 'comment';
+	}
+	
+	$scope.toStaff = function() {
+		$scope.tab = 'staff';
+	}
+
+});
